b885ce510c674a23f17f605c2fe2a374953c0074
Sivaguru Ravi <Sivaguru.Ravi@mindtree.com>
2018-05-14 01:45:13 +0530

    prop

---
+++ src/com/restful/electronicshopping/entity/PriceRange.java
@@ -0,0 +1,22 @@
+package com.restful.electronicshopping.entity;
+
+public class PriceRange {
+	int low;
+	int high;
+	public int getLow() {
+		return low;
+	}
+	public void setLow(int low) {
+		this.low = low;
+	}
+	public int getHigh() {
+		return high;
+	}
+	public void setHigh(int high) {
+		this.high = high;
+	}
+	public PriceRange() {
+		super();
+	}
+	
+}
